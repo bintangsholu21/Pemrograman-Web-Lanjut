a1675b9511bc752fe28512753d1048e6831c533f
Muhammad Bintang Sholu Firmansyah <bintangsholu@gmail.com>
2023-06-19 19:26:17 +0700

    add Jobsheet 10

---
+++ Pertemuan-10/praktikum1/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ArticleController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('mahasiswas', MahasiswaController::class);
+Route::resource('articles', ArticleController::class);
+Route::get('print_article', [ArticleController::class, 'print_pdf']);
+
